06b881d02b78d3a324c7566e44230e7cc3107730
Wei Yen <weiyen.lee87@gmail.com>
2013-02-02 00:07:33 +0800

    Initial commit

---
+++ chinese-calendar.py
@@ -0,0 +1,10 @@
+class ChineseDate():
+    def __init__(solar_year, solar_month, solar_date):
+        self.lunar_year = solar_year
+        self.lunar_month
+        pass
+
+    def __repr__():
+        pass
+
+
---
+++ constants.py
@@ -0,0 +1,37 @@
+zodiac = [ 'rat', 
+           'ox', 
+           'tiger', 
+           'rabbit', 
+           'dragon', 
+           'snake', 
+           'horse', 
+           'goat',
+           'monkey',
+           'rooster',
+           'dog',
+           'pig']
+
+heavenly_stems = ['jia',
+                  'yi',
+                  'bing',
+                  'ding',
+                  'wu',
+                  'ji',
+                  'geng',
+                  'xin',
+                  'ren', 
+                  'gui']
+
+earthly_branches = ['zi',
+                    'chou',
+                    'yin',
+                    'mao',
+                    'chen',
+                    'si',
+                    'wu',
+                    'wei',
+                    'shen',
+                    'you']
+
+
+
