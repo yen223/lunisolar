dfe149e24b6d2ee80c65f927e8c6926685f27b6c
Wei Yen <weiyen.lee87@gmail.com>
2013-02-02 12:13:03 +0800

    Added branch prediction

--- chinese_calendar.py
+++ chinese_calendar.py
@@ -1,30 +1,41 @@
 from constants import zodiac
-from datetime import date as base_date
+from datetime import date as solar_date
 from exceptions import ValueError
 
-class LunarDate(base_date):
+class LunarDate():
     def __init__(self, lunar_year, lunar_month=1, lunar_day=1):
-        print "I'm in the init!"
         if not 1 <= lunar_month <= 13:
             raise ValueError, 'Lunar month must be between 1 and 13.'
         if not 1 <= lunar_day <= 30:
             raise ValueError, 'Lunar day must be between 1 and 30.'
 
-        self.year = lunar_year
-        self.month = 1
-        self.day = 1
         self.lunar_year = lunar_year
         self.lunar_month = lunar_month
         self.lunar_day = lunar_day
-        super(LunarDate, self).__init__(lunar_year, month=10, day=10)
 
-    def __repr__(self):
+        self.solar_date = solar_date(lunar_year, lunar_month, lunar_day)
+
+        self._branch = self.get_branch(self.lunar_year)
+
+    def __str__(self):
         return 'Year {0}, {1} month, {2} day'.format(self.lunar_year,
                                                      ordinal(self.lunar_month),
                                                      ordinal(self.lunar_day))
 
+    __repr__ = __str__
+
+    @staticmethod
+    def get_branch(lunar_year):
+        return (lunar_year - 4) % 12
+        
+
+    @staticmethod
+    def to_solar_date(lunar_year, lunar_month, lunar_day):
+        solar_year = lunar_year
+
+    @property
     def zodiac(self):
-        return zodiac[3]
+        return zodiac[self._branch]
 
 def ordinal(value):
     str_val = str(value)
--- test_chinese_calendar.py
+++ test_chinese_calendar.py
@@ -3,15 +3,23 @@ from chinese_calendar import LunarDate, ordinal
 class LunarDateTestCases(unittest.TestCase):
     def setUp(self):
         self.date_30_12_1924 = LunarDate(1924, 12, 30)
+        self.date_15_1_2014 = LunarDate(2014, 1, 15)
 
     def test_year(self):
-        self.assertEqual(self.date_30_12_1924.year, 1924)
-    
+        self.assertEqual(self.date_30_12_1924.lunar_year, 1924)
+        self.assertEqual(self.date_15_1_2014.lunar_year, 2014)
+
     def test_month(self):
-        self.assertEqual(self.date_30_12_1924.month, 12)
+        self.assertEqual(self.date_30_12_1924.lunar_month, 12)
+        self.assertEqual(self.date_15_1_2014.lunar_month, 1)
 
     def test_day(self):
-        self.assertEqual(self.date_30_12_1924.day, 30)
+        self.assertEqual(self.date_30_12_1924.lunar_day, 30)
+        self.assertEqual(self.date_15_1_2014.lunar_day, 15)
+
+    def test_earthly_branch(self):
+        self.assertEqual(self.date_30_12_1924._branch, 0)
+        self.assertEqual(self.date_15_1_2014._branch, 6)
 
     def test_convert_to_western(self):
         pass
