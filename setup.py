45c88579518266bc6ea4946ba83e2f5ff70334a8
Wei Yen <weiyen.lee87@gmail.com>
2013-02-12 02:29:54 +0800

    Upped version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
         name = "lunisolar",
         packages = ["lunisolar", "pycalcal"],
         install_requires=requires,
-        version = "0.1.0",
+        version = "0.1.1",
         description = "A library to handle the Chinese calendar",
         long_description=open('README.rst').read(),
         license=open('LICENSE').read(),
